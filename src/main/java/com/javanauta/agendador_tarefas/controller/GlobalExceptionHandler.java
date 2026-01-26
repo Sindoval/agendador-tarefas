126078bd986fa584411623c807ac6e341d10f0e8
Sindoval <sindoval321@gmail.com>
2026-01-26 16:44:19 -0300

    Implementacao da GlobalExceptionHandler

---
+++ src/main/java/com/javanauta/agendador_tarefas/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.javanauta.agendador_tarefas.controller;
+
+import com.javanauta.agendador_tarefas.infrastructure.exceptions.ResourceNotFoundException;
+import com.javanauta.agendador_tarefas.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(UnauthorizedException.class)
+  public ResponseEntity<String> handleResourceNotFoundException(UnauthorizedException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+  }
+}
